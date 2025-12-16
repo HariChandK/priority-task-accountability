2b58aff27d6557cd034e0e6a4073cfa699067992
Hari Chandana <harichankona@gmail.com>
2025-12-16 02:34:05 -0500

    Set up JUnit5 and fix test package

---
+++ src/test/java/com/hk/todo/AppTest.java
@@ -0,0 +1,13 @@
+package com.hk.todo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void sanityTest() {
+        assertTrue(true);
+    }
+}
